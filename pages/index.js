a1504f2c561838cd914c438a215e66d70616fbbc
hippopotacos <hippopotacos@gmail.com>
2021-01-26 22:58:14 +0900

    CSS Modulesのテスト

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
